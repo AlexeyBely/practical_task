f0c14f7d5119a3c4c5669b9187f513be61fa78f5
Alexey Beloglazov <alexey.bely.1982@yandex.ru>
2023-09-22 23:15:40 +0700

    add pyspark

--- src/main.py
+++ src/main.py
@@ -2,16 +2,26 @@ import logging
 import math
 
 from geometric import CalcShapes
-from core.config import settings
+from products.create_products import (spark_session, create_categories, create_products, 
+                                      create_product_category)
+from products.read_products import select_products
 
 
 logger = logging.getLogger(__name__)
         
 
 def main():
+    # geometric
     shapes = CalcShapes()
-    aaa = shapes.calc_area(CalcShapes.Shapes.TRIANGLE, 3, 4, 5)
-    print(f'{aaa}')
+    area = shapes.calc_area(CalcShapes.Shapes.TRIANGLE, 3, 4, 5)
+    print(f'area: {area}')
+    # products
+    spark = spark_session()
+    prod = create_products(spark)
+    cat = create_categories(spark)
+    prod_cat = create_product_category(spark)
+    products = select_products(spark, prod, cat, prod_cat)
+    products.show()
 
 
 if __name__ == '__main__':
